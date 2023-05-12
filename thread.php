149c650d0b327eb8aae6ef0e59de247cc80baa61
sourabhshakywal333 <113864949+sourabhshakywal333@users.noreply.github.com>
2023-05-13 00:58:38 +0530

    Update thread.php

--- thread.php
+++ thread.php
@@ -10,23 +10,30 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <style>
-    #ques {
-        min-height: 433px;
-    }
+        #ques{
+            min-height: 433px;
+        }
     </style>
-    <title>Hacking_tools.com</title>
+    <title>Welcome to Hacking_tools.com</title>
 </head>
 
 <body>
-    <?php include 'partials/dbconnect.php';?>
     <?php include 'partials/header.php';?>
+    <?php include 'partials/dbconnect.php';?>
     <?php
-    $id = $_GET['catid'];
-    $sql = "SELECT * FROM `hacking_tools` WHERE id =$id"; 
+    $id = $_GET['threadid'];
+    $sql = "SELECT * FROM `threads` WHERE thread_id=$id"; 
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_assoc($result)){
-        $created = $row['name'];
-        $createddesc = $row['description'];
+        $title = $row['thread_title'];
+        $desc = $row['thread_desc'];
+        $thread_user_id = $row['thread_user_id'];
+
+        // Query the users table to find out the name of OP
+        $sql2 = "SELECT user_email FROM `users` WHERE sno='$thread_user_id'";
+        $result2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_assoc($result2);
+        $posted_by = $row2['user_email'];
     }
     
     ?>
@@ -35,23 +42,17 @@
     $showAlert = false;
     $method = $_SERVER['REQUEST_METHOD'];
     if($method=='POST'){
-        // Insert into thread db
-        $th_title = $_POST['title'];
-        $th_desc = $_POST['desc'];
-
-        $th_title = str_replace("<", "&lt;", $th_title);
-        $th_title = str_replace(">", "&gt;", $th_title); 
-
-        $th_desc = str_replace("<", "&lt;", $th_desc);
-        $th_desc = str_replace(">", "&gt;", $th_desc); 
-
+        //Insert into comment db
+        $comment = $_POST['comment']; 
+        $comment = str_replace("<", "&lt;", $comment);
+        $comment = str_replace(">", "&gt;", $comment); 
         $sno = $_POST['sno']; 
-        $sql = "INSERT INTO `threads` (`thread_title`, `thread_desc`, `thread_cat_id`, `thread_user_id`, `timestamp`) VALUES ( '$th_title', '$th_desc', '$id', '$sno', current_timestamp())";
+        $sql = "INSERT INTO `comments` ( `comment_content`, `thread_id`, `comment_by`, `comment_time`) VALUES ('$comment', '$id', '$sno', current_timestamp())";
         $result = mysqli_query($conn, $sql);
         $showAlert = true;
         if($showAlert){
             echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <strong>Success!</strong> Your thread has been added! Please wait for community to respond
+                        <strong>Success!</strong> Your comment has been added!
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -64,90 +65,83 @@
     <!-- Category container starts here -->
     <div class="container my-4">
         <div class="jumbotron">
-            <h1 class="display-4">Welcome to <?php echo $created;?></h1>
-            <p class="lead"> <?php echo $createddesc;?></p>
+            <h1 class="display-4"><?php echo $title;?></h1>
+            <p class="lead">  <?php echo $desc;?></p>
             <hr class="my-4">
-            <p>This is a peer to peer forum. No Spam / Advertising / Self-promote in the forums is not allowed. Do not
-                post copyright-infringing material. Do not post “offensive” posts, links or images. Do not cross post
-                questions. Remain respectful of other members at all times.</p>
-            <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a>
+            <p>This is a peer to peer forum. No Spam / Advertising / Self-promote in the forums is not allowed. Do not post copyright-infringing material. Do not post “offensive” posts, links or images. Do not cross post questions. Remain respectful of other members at all times.</p>
+            <p>Posted by: <em><?php echo $posted_by; ?></em></p>
         </div>
     </div>
 
-    <?php 
+     <?php 
     if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){ 
     echo '<div class="container">
-            <h1 class="py-2">Start a Discussion</h1> 
-            <form action="'. $_SERVER["REQUEST_URI"] . '" method="post">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Problem Title</label>
-                    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
-                    <small id="emailHelp" class="form-text text-muted">Keep your title as short and crisp as
-                        possible</small>
-                </div>
+        <h1 class="py-2">Post a Comment</h1> 
+        <form action= "'. $_SERVER['REQUEST_URI'] . '" method="post"> 
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Type your comment</label>
+                <textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
                 <input type="hidden" name="sno" value="'. $_SESSION["sno"]. '">
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Ellaborate Your Concern</label>
-                    <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-success">Submit</button>
-            </form>
-        </div>';
+            </div>
+            <button type="submit" class="btn btn-success">Post Comment</button>
+        </form> 
+    </div>';
     }
     else{
         echo '
+        
         <div class="container">
-        <h1 class="py-2">Start a Discussion</h1> 
-           <p class="lead">You are not logged in. Please login to be able to start a Discussion</p>
+        <h1 class="py-2">Post a Comment</h1> 
+           <p class="lead">You are not logged in. Please login to be able to post comments.</p>
         </div>
         ';
     }
 
     ?>
-    
+
+
     <div class="container mb-5" id="ques">
-        <h1 class="py-2">Browse Questions</h1>
-    <?php
-    $id = $_GET['catid'];
-    $sql = "SELECT * FROM threads WHERE thread_cat_id=$id";
+        <h1 class="py-2">Discussions</h1>
+       <?php
+    $id = $_GET['threadid'];
+    $sql = "SELECT * FROM `comments` WHERE thread_id=$id"; 
     $result = mysqli_query($conn, $sql);
     $noResult = true;
     while($row = mysqli_fetch_assoc($result)){
         $noResult = false;
-        $id = $row['thread_id'];
-        $title = $row['thread_title'];
-        $desc = $row['thread_desc']; 
-        $thread_time = $row['timestamp']; 
-        $thread_user_id = $row['thread_user_id']; 
+        $id = $row['comment_id'];
+        $content = $row['comment_content']; 
+        $comment_time = $row['comment_time']; 
+        $thread_user_id = $row['comment_by']; 
+
         $sql2 = "SELECT user_email FROM `users` WHERE sno='$thread_user_id'";
         $result2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($result2);
-        
-
-
 
         echo '<div class="media my-3">
             <img src="img/userdefault.png" width="54px" class="mr-3" alt="...">
-            <div class="media-body">'.
-             '<h5 class="mt-0"> <a class="text-dark" href="threads.php?threadid=' . $id. '">'. $title . ' </a></h5>
-                '. $desc . ' </div>'.'<div class="font-weight-bold my-0"> Asked by: '. $row2['user_email'] . ' at '. $thread_time. '</div>'.
-        '</div>';
+            <div class="media-body">
+               <p class="font-weight-bold my-0">'. $row2['user_email'] .' at '. $comment_time. '</p> '. $content . '
+            </div>
+        </div>';
 
         }
+        // echo "this is good";
         // echo var_dump($noResult);
         if($noResult){
             echo '<div class="jumbotron jumbotron-fluid">
                     <div class="container">
-                        <p class="display-4">No Threads Found</p>
-                        <p class="lead"> Be the first person to ask a question</p>
+                        <p class="display-4">No Comments Found</p>
+                        <p class="lead"> Be the first person to comment</p>
                     </div>
                  </div> ';
         }
-    ?>
+    
+    ?> 
 
     </div>
 
-    <?php include 'partials/footer.php';?>
+    <?php include 'partials/_footer.php';?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
